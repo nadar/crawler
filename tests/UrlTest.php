d4ceb7b9e967bab518d631431ee542ceb678e7f7
Basil Suter <git@nadar.io>
2020-09-09 07:09:26 +0000

    add url tests

--- tests/UrlTest.php
+++ tests/UrlTest.php
@@ -16,5 +16,16 @@ class UrlTest extends CrawlerTestCase
     {
         $url = new Url('https://luya.io/files/thisismysuper.pdf');
         $this->assertSame('thisismysuper.pdf', $url->getPathFileName());
+        $this->assertSame('pdf', $url->getPathExtension());
+        $this->assertSame('https://luya.io/files/thisismysuper.pdf', $url->getNormalized());
+        $this->assertSame('luya.iofiles/thisismysuper.pdf', $url->getUniqueKey());
+        $this->assertSame('luya.io', $url->getHost());
+        $this->assertSame('/files/thisismysuper.pdf', $url->getPath());
+        $this->assertSame('thisismysuper.pdf', $url->getPathFileName());
+        $this->assertSame(false, $url->getQuery());
+        $this->assertSame('https', $url->getScheme());
+        $this->assertSame(true, $url->isValid());
+        $this->assertTrue($url->sameHost(new Url('https://luya.io')));
+        $this->assertFalse($url->sameHost(new Url('https://nadar.io')));
     }
 }
