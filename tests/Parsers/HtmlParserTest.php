2478235cc95610250041fdf44594ad7c498f6337
Basil Suter <git@nadar.io>
2020-09-09 16:17:08 +0000

    add ignore tag in html test

--- tests/Parsers/HtmlParserTest.php
+++ tests/Parsers/HtmlParserTest.php
@@ -89,7 +89,7 @@ class HtmlParserTest extends CrawlerTestCase
         <body>
         <div>
             <h1>Example Domain</h1>
-            <p>This domain is for use in illustrative examples in documents. You may use this
+            <p>This domain is for use in [CRAWL_IGNORE]illustrative[/CRAWL_IGNORE] examples in documents. You may use this
             domain in literature without prior coordination or asking for permission.</p>
             <p><a href="https://www.iana.org/domains/example">More information...</a></p>
         </div>
@@ -101,7 +101,7 @@ class HtmlParserTest extends CrawlerTestCase
         $parser->stripTags = false;
         $result = $parser->run($job, $requestResponse);
 
-        $this->assertSame('<body> <div> <h1>Example Domain</h1> <p>This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.</p> <p><a href="https://www.iana.org/domains/example">More information...</a></p> </div> </body>', $result->content);
+        $this->assertSame('<body> <div> <h1>Example Domain</h1> <p>This domain is for use in examples in documents. You may use this domain in literature without prior coordination or asking for permission.</p> <p><a href="https://www.iana.org/domains/example">More information...</a></p> </div> </body>', $result->content);
 
         $this->assertSame('Example Domain', $result->title);
 
