79c55528a04e8fbad222d1b03662f60fc4ed258a
Basil Suter <git@nadar.io>
2020-09-12 11:27:08 +0000

    add async runner callable

---
+++ tests/Runners/AsyncRunnerTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Nadar\Crawler\Tests;
+
+use Nadar\Crawler\Crawler;
+use Nadar\Crawler\Runners\AsyncRunner;
+use Nadar\Crawler\Storage\ArrayStorage;
+
+class AsyncRunnerTest extends CrawlerTestCase
+{
+    public function testAsyncRunnerCallable()
+    {
+        $runner = new AsyncRunner(function() {
+            // run callable    
+        });
+
+        $crawler = new Crawler('localhost', new ArrayStorage, $runner);
+        $this->assertEmpty($runner->afterRun($crawler));
+        
+    }
+}
